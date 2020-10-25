c69b86059ea3db04354c3c6f5e0c9df7f8e71f32
James Franco <jdfranco@unomaha.edu>
2020-10-25 17:14:36 -0500

    First Commit, Admin page and models setup

--- garagems/settings.py
+++ garagems/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'users',
     'crispy_forms',
     'clients',
+    'django.contrib.postgres',
 
 ]
 
@@ -79,8 +81,10 @@ WSGI_APPLICATION = 'garagems.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'garagemsDB',
+        'USER': 'postgres',
+        'PASSWORD': 'maverick'
     }
 }
 
@@ -120,7 +124,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL ='/media/'
+
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+django_heroku.settings(locals())
