773d9b7dc2a41fc7586967a4403dc269476e635d
James Franco <jdfranco@unomaha.edu>
2020-11-10 16:56:24 -0600

    Working client list, non functional logo

--- garagems/settings.py
+++ garagems/settings.py
@@ -124,11 +124,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/static/garagems/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL ='/media/'
-
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
